6f9b612a8e45dc2f788b25c63e4a447a9c683fd0
M. Fazri Nizar <anon6372098.id@gmail.com>
2025-05-12 01:08:07 +0700

    Feat: add Config code

---
+++ include/Config.h
@@ -0,0 +1,14 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+void loadConfig();
+
+extern const char *DEVICE_ID;
+extern const char *SERVER_URL;
+extern const char *WS_URL;
+extern const char *API_KEY;
+extern const char *WIFI_SSID;
+extern const char *WIFI_PASSWORD;
+extern const int *TIMEZONE;
+
+#endif
